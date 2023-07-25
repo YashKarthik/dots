c15064472a9c19c2946269b1557cecf2e31c9570
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-07-25 22:26:28 +0530

    C debuggger

--- .config/nvim/lua/yash/debugging.lua
+++ .config/nvim/lua/yash/debugging.lua
@@ -1,9 +1,6 @@
-
 local dap, dapui = require("dap"), require("dapui")
-
-require('dap-go').setup()
-require('dapui').setup()
 dapui.setup()
+require('telescope').load_extension('dap')
 
 -- Auto open/close dap ui
 dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -16,3 +13,35 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+
+--              Languages
+
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+
+
+-- Go
+require('dap-go').setup()
+
+-- C, C++, Rust
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/local/opt/llvm/bin/lldb-vscode', -- found using $(brew --prefix llvm)/bin
+  name = 'lldb'
+}
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+  },
+}
+dap.configurations.c = dap.configurations.cpp
